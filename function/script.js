252eac6d9a397488ab71fa00ec2c8a55db56393c
SarthakBahuguna2000 <109979350+SarthakBahuguna2000@users.noreply.github.com>
2022-08-18 16:37:39 +0530

    Update script.js

--- function/script.js
+++ function/script.js
@@ -1,15 +1,21 @@
-$(document).ready(function(){
-    $("#btn1").click(function(){
-        var x=factorial(6);
-        document.getElementById("btn1").innerHTML=x;
+$(document).ready(function () {
+    $("#btn1").click(function () {
+        var x = factorial(6);
+        document.getElementById("btn1").innerHTML = x;
     });
 });
 
 
-$(document).ready(function(){
-    $("#btn2").click(function(){
-        var x=add(6);
-        document.getElementById("btn2").innerHTML=x;
+$(document).ready(function () {
+    $("#btn2").click(function () {
+        var x = add(6);
+        document.getElementById("btn2").innerHTML = x;
+    });
+});
+$(document).ready(function () {
+    $("#btn3").click(function () {
+        var x = diff(10);
+        document.getElementById("btn3").innerHTML = x;
     });
 });
 
@@ -17,12 +23,26 @@ $(document).ready(function(){
 
 
 
-
+function factorial(x) {
+    if (x == 0) {
+        return 1;
+    }
+    else {
+        return x * factorial(x - 1);
+    }
+}
 
 function add(x) {
     let counter = 5;
-    function plus() {x+=counter;}
+    function plus() { x += counter; }
     plus();
-    plus();   
+    plus();
     return x;
-  }
+}
+function diff(x) {
+    let counter = 10;
+    function plus() { x -= counter; }
+    diff();
+    diff();
+    return x;
+}
